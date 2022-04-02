3e3a4106e48058ab9e9cf1f4a7f4b3d1f64cc2f6
adir-jscode <adir.jscode@gmail.com>
2022-04-02 11:48:35 +0600

    load data based on dynamic route

--- src/Components/FriendDetails/FriendDetails.js
+++ src/Components/FriendDetails/FriendDetails.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
     const params = useParams();
+    const { friendId } = params;
+    // const { friendId } = useParams();
+    const [friend, setFriend] = useState({})
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
+        fetch(url)
+            .then(response => response.json())
+        .then(data=>setFriend(data))
+    },[])
     return (
         <div>
-            <h1>Details showing { params.friendId}</h1> 
+            <h1>Details showing {params.friendId}</h1>
+            <h1> userName: {friend.username}</h1>
+            <p>ID : {friend.id}</p>
+            <p><small>Phone : {friend.phone}</small></p>
+            <p>Address : {friend.address?.city}</p>
+            <p>LAT: { friend.address?.geo.lat}</p>
         </div>
     );
 };
